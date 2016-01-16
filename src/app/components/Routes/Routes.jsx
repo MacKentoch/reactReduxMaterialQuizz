37c78ffd30f9555da0912c974f0348751d78dbc9
MacKentoch <erwan.datin@gmail.com>
2016-01-16 14:48:39 +0100

    chore : redux-simple-router - redux-devTools - route is in redux state (can revert, commit...)

--- src/app/components/Routes/Routes.jsx
+++ src/app/components/Routes/Routes.jsx
@@ -4,36 +4,37 @@ import Router, {
   IndexRoute,
   browserHistory 
  }                              from 'react-router';
-import { 
-  compose, 
-  createStore, 
-  combineReducers, 
-  applyMiddleware 
-}                               from 'redux';
+//import createBrowserHistory   from 'history/lib/createBrowserHistory';
 import { Provider }             from 'react-redux';
 import { 
   syncHistory, 
+  syncReduxAndRouter,
   routeReducer 
 }                               from 'redux-simple-router';
+
 import ReactMaterialQuizz       from '../ReactMaterialQuizz/ReactMaterialQuizz.jsx!jsx'; 
 import Home                     from '../Home/Home.jsx!jsx';  
 import Quiz                     from '../Quiz/Quiz.jsx!jsx';  
-
+ 
 // IMPORTANT : PRODUCTION : Store WITHOUT redux-devtools :
 //import configureStore           from '../../redux/store/quizStore.prod.jsx!jsx';
 
 // IMPORTANT : DEVELOPEMENT : Store WITH redux-devtools :
-import configureStore           from '../../redux/store/quizStore.dev.jsx!jsx';
+import configureStore, {
+  history
+}                               from '../../redux/store/quizStore.dev.jsx!jsx';
 
 
 
 const store = configureStore();
+// Required for replaying actions from devtools to work
+
 
 // INFO : Route is written as a "stateless functionnal component" (= ES6 arrow function is enough to describe it - see React V0.14 major changes) 
 export const Routes = ()=>{
   return (
     <Provider store={store}>
-      <Router >
+      <Router history={history}>
         <Route path="/" component={ReactMaterialQuizz}>
           <IndexRoute component={Home} />
           <Route path="quiz" component={Quiz} />
--- src/app/redux/store/quizStore.dev.jsx
+++ src/app/redux/store/quizStore.dev.jsx
@@ -1,36 +1,61 @@
 import { 
   createStore, 
   applyMiddleware, 
+  combineReducers,
   compose 
-}                         from 'redux';
-import thunkMiddleware    from 'redux-thunk';
-import createLogger       from 'redux-logger';
-import { persistState }   from 'redux-devtools';
-import quizReducer        from '../reducers/quizReducers.jsx!jsx';
-import DevTools           from '../../components/DevTools/DevTools.jsx!jsx';
+}                           from 'redux';
+import thunkMiddleware      from 'redux-thunk';
+import createLogger         from 'redux-logger';
+import { persistState }     from 'redux-devtools';
+import quizReducer          from '../reducers/quizReducers.jsx!jsx';
+import DevTools             from '../../components/DevTools/DevTools.jsx!jsx';
+import {
+  routeReducer,
+  syncHistory,
+  syncReduxAndRouter
+}                           from 'redux-simple-router';
+// import { browserHistory }   from 'react-router';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
 
+
+//combine reducers
+const reducer = combineReducers(Object.assign({}, quizReducer, {
+  routing : routeReducer
+}));
+
+
+// logger middleware
 const loggerMiddleware = createLogger({
   level     : 'info',
   collapsed : true
 });
 
+export const history = createBrowserHistory();
+
+// Sync dispatched route actions to the history
+const reduxRouterMiddleware = syncHistory(history)
+
+// compose middlewares
 const finalCreateStore = compose(
-  applyMiddleware(thunkMiddleware, loggerMiddleware),
+  applyMiddleware(thunkMiddleware, loggerMiddleware, reduxRouterMiddleware),
   // Required! Enable Redux DevTools with the monitors you chose
   DevTools.instrument() // ,
   // Optional. Lets you write ?debug_session=<key> in address bar to persist debug sessions
   // persistState(getDebugSessionKey())
 )(createStore);
 
-function getDebugSessionKey() {
-  // You can write custom logic here!
-  // By default we try to read the key from ?debug_session=<key> in the address bar
-  const matches = window.location.href.match(/[?&]debug_session=([^&]+)\b/);
-  return (matches && matches.length > 0)? matches[1] : null;
-}
+
+
+// function getDebugSessionKey() {
+//   // You can write custom logic here!
+//   // By default we try to read the key from ?debug_session=<key> in the address bar
+//   const matches = window.location.href.match(/[?&]debug_session=([^&]+)\b/);
+//   return (matches && matches.length > 0)? matches[1] : null;
+// }
+
 
 export default function configureStore(initialState) {
-  const store = finalCreateStore(quizReducer, initialState);
+  const store   = finalCreateStore(reducer, initialState);
 
   // // Hot reload reducers (requires Webpack or Browserify HMR to be enabled)
   // if (module.hot) {
@@ -38,6 +63,10 @@ export default function configureStore(initialState) {
   //     store.replaceReducer(require('../reducers')/*.default if you use Babel 6+ */)
   //   );
   // }
+
+
+  // Required for replaying actions from devtools to work
+  reduxRouterMiddleware.listenForReplays(store)
 
   return store;
 }
